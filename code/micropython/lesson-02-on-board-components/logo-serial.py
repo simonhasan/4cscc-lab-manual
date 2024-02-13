589c31498471840b5e24b1bb7ec73c2da3262f18
Simon <25673729+simonhasan@users.noreply.github.com>
2024-02-13 12:45:39 -0700

    Updated content

--- code/micropython/lesson-02-on-board-components/logo-serial.py
+++ code/micropython/lesson-02-on-board-components/logo-serial.py
@@ -4,5 +4,5 @@ from microbit import *
 # Code in a 'while True:' loop repeats forever
 while True:
     # Print the state of button A and button B
-    print('A:', button_a.is_pressed(), 'B:', button_b.is_pressed())
+    print('Logo touched:', pin_logo.is_touched())
     sleep(100) # Wait for 100ms (0.1 seconds)
