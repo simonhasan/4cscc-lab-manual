8ccb4b94ffedc502c848e959306b3e4aa9a5580e
Simon <25673729+simonhasan@users.noreply.github.com>
2024-02-12 23:37:05 -0700

    Updated content

---
+++ code/micropython/lesson-01-display/display-custom-image-01.py
@@ -0,0 +1,12 @@
+# Imports go at the top
+from microbit import *
+
+# Create a custom image of a coyote
+coyote = Image("99099:"
+               "09990:"
+               "90909:"
+               "99999:"
+               "00900")
+
+# Display the custom image
+display.show(coyote)
---
+++ code/micropython/lesson-01-display/display-custom-image-02.py
@@ -0,0 +1,8 @@
+# Imports go at the top
+from microbit import *
+
+# Create a custom image of a coyote
+coyote = Image("99099:09990:90909:99999:00900")
+
+# Display the custom image
+display.show(coyote)
