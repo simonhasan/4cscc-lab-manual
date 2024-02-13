4ca1c5a5bf52eb0c5efb104ce79d4dff3da6e134
Simon <25673729+simonhasan@users.noreply.github.com>
2024-02-13 12:49:09 -0700

    Updated content

--- code/micropython/lesson-02-on-board-components/sound-serial.py
+++ code/micropython/lesson-02-on-board-components/sound-serial.py
@@ -3,6 +3,6 @@ from microbit import *
 
 # Code in a 'while True:' loop repeats forever
 while True:
-    # Print the state of the logo touch sensor
-    print('Logo touched:', pin_logo.is_touched())
+    # Print the sound level
+    print('Sound level:', microphone.sound_level())
     sleep(100) # Wait for 100ms (0.1 seconds)
